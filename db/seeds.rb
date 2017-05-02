804eb2ab88d867088f65684fabf0bebad3031dce
Niaja <nfarve@mit.edu>
2017-05-02 18:03:51 -0400

    updating seeds to prepopulate database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+SI_list = 
+	[["minute", "min",60, "s"],["hour", "h", 3600, "s"], ["day", "d", 86400, "s"], ["degree", "°", Math::PI/180, "rad" ], 
+	["'", "'", Math::PI/10800, "rad"], ["second", '"', Math::PI/648000, "rad"] ,["hectare", "ha", 10000, "m\u{00B2}"], 
+	["litre", "L",0.001,"m\u{00B3}"],["tonne", "t", 1000, "kg"]
+]
+
+SI_list.each do |name, symbol, factor, unit|
+	Conversion.create(name: name, symbol: symbol, factor: factor, unit: unit)
+end
